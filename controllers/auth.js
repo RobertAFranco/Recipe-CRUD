a9fa4598225c6388dc108e4af45d3eacae2b454b
RobertAFranco <bigbobontheradio@gmail.com>
2024-08-14 01:57:37 -0400

    controllers updated

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,81 +1,80 @@
-import express from "express";
-import bcrypt from 'bcrypt';
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
 
-import User from '../models/user.js'
+const User = require('../models/user.js');
 
-const authRouter = express.Router();
-
-authRouter.get('/sign-up', async (req, res) => {
-  res.render('auth/sign-up.ejs', {
-    user: req.session.user
-  });
+router.get('/sign-up', (req, res) => {
+  res.render('auth/sign-up.ejs');
 });
 
-authRouter.get('/sign-in', async (req, res) => {
-  res.render('auth/sign-in.ejs', {
-    user: req.session.user
-  });
+router.get('/sign-in', (req, res) => {
+  res.render('auth/sign-in.ejs');
 });
 
-authRouter.post('/sign-up', async (req, res) => {
-  try {
-    const user = await User.findOne({ username: req.body.username });
-
-    if (user) {
-      return res.send('User already exists');
-    }
-
-    if (req.body.password !== req.body.confirmPassword) {
-      return res.send('Password does not match Confirm Password');
-    }
-
-    // Hash the provided password using bcrypt, salt 10 times
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    req.body.password = hashedPassword;
-
-    // Create a new User
-    const newUser = await User.create(req.body);
-    
-    res.send(`
-      <h1>Thanks for signing up, ${newUser.username}!</h1>
-      <a href="/">Go to Home Page, you can now sign in!</a>
-    `);
-  } catch (error) {
-    console.error('Error during sign-up:', error);
-    res.status(500).send('Internal Server Error.');
-  }
-});
-
-authRouter.post('/sign-in', async (req, res) => {
-  try {
-    // Check if the user exists
-    const user = await User.findOne({ username: req.body.username });
-
-    if (!user) {
-      return res.send('User either does not exist, or you have provided the wrong credentials');
-    }
-
-    const validPassword = await bcrypt.compare(req.body.password, user.password);
-
-    if (!validPassword) {
-      return res.send('Error, the password was wrong!');
-    }
-
-    req.session.user = {
-      username: user.username,
-      _id: user._id
-    };
-
-    res.redirect('/');
-  } catch (error) {
-    console.error('Was not able to sign in', error);
-    res.status(500).send('Internal Server Error.');
-  }
-});
-
-authRouter.get('/sign-out', async (req, res) => {
+router.get('/sign-out', (req, res) => {
   req.session.destroy();
   res.redirect('/');
 });
 
-export default authRouter;
+router.post('/sign-up', async (req, res) => {
+  try {
+    // Check if the username is already taken
+    const userInDatabase = await User.findOne({ username: req.body.username });
+    if (userInDatabase) {
+      return res.send('Username already taken.');
+    }
+  
+    // Username is not taken already!
+    // Check if the password and confirm password match
+    if (req.body.password !== req.body.confirmPassword) {
+      return res.send('Password and Confirm Password must match');
+    }
+  
+    // Must hash the password before sending to the database
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+    req.body.password = hashedPassword;
+  
+    // All ready to create the new user!
+    await User.create(req.body);
+  
+    res.redirect('/auth/sign-in');
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+router.post('/sign-in', async (req, res) => {
+  try {
+    // First, get the user from the database
+    const userInDatabase = await User.findOne({ username: req.body.username });
+    if (!userInDatabase) {
+      return res.send('Login failed. Please try again.');
+    }
+  
+    // There is a user! Time to test their password with bcrypt
+    const validPassword = bcrypt.compareSync(
+      req.body.password,
+      userInDatabase.password
+    );
+    if (!validPassword) {
+      return res.send('Login failed. Please try again.');
+    }
+  
+    // There is a user AND they had the correct password. Time to make a session!
+    // Avoid storing the password, even in hashed format, in the session
+    // If there is other data you want to save to `req.session.user`, do so here!
+    req.session.user = {
+      username: userInDatabase.username,
+      _id: userInDatabase._id
+    };
+  
+    res.redirect('/');
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+module.exports = router;
--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,17 +1,154 @@
+const express = require('express');
+const router = express.Router();
+const Recipe = require('../models/recipe');
 
+// GET /Recipes
+router.get('/', async (req, res) => {
+  try {
+    const recipes = await Recipe.find({}).populate('owner');
+    res.render('recipes/index', { recipes });
+  } catch (err) {
+    console.error('Error fetching recipes:', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
-import Recipe from '../models/recipe.js'
+// GET /recipes/new
+router.get('/new', (req, res) => {
+  res.render('recipes/new');
+});
 
-const index = async (req, res) => {
-    const allRecipes = await Recipe.find();
-    res.render("recipes/index.ejs", {recipes: allRecipes});
-}
+// GET /recipes/:recipeId
+router.get('/:recipeId', async (req, res) => {
+  try {
+    const recipe = await Recipe.findById(req.params.recipeId).populate('owner');
+    
+    if (!recipe) {
+      return res.status(404).send('Recipe not found');
+    }
 
-const newRecipes = async (req, res) => {
-    const allRecipes = await Recipe.find();
-    res.render("recipes/new.ejs", {recipes: allRecipes});
-}
+    const userHasFavorited = recipe.favoritedByUsers.some(user => user.equals(req.session.user._id));
 
-export {
-    index, newRecipes
-}
+    res.render('recipes/show', { 
+      recipe, 
+      userHasFavorited 
+    });
+  } catch (error) {
+    console.error('Error fetching recipe:', error);
+    res.redirect('/');
+  }
+});
+
+// POST /recipes
+router.post('/', async (req, res) => {
+  try {
+    const { name, ingredients, instructions, tips} = req.body;
+
+    if (!name || !ingredients || !instructions || !tips) {
+      return res.status(400).send('All fields are required.');
+    }
+
+    if (!req.session.user || !req.session.user._id) {
+      return res.status(401).send('User not authenticated');
+    }
+
+    const newRecipe = new Recipe({
+      name,
+      ingredients,
+      instructions,
+      tips,
+      owner: req.session.user._id
+    });
+
+    await newRecipe.save();
+    res.redirect('/recipes');
+  } catch (error) {
+    console.error('Error creating recipe:', error);
+    res.status(500).send('Error creating recipe.');
+  }
+});
+
+// DELETE /recipes/:recipesId
+router.delete('/:recipeId', async (req, res) => {
+  try {
+    const recipe = await Recipe.findById(req.params.recipeId);
+
+    if (!recipe) {
+      return res.status(404).send('Recipe not found.');
+    }
+
+    if (recipe.owner.equals(req.session.user._id)) {
+      await recipe.deleteOne();
+      res.redirect('/recipes');
+    } else {
+      res.status(403).send("You don't have permission to do that.");
+    }
+  } catch (error) {
+    console.error('Error deleting recipe:', error);
+    res.redirect('/');
+  }
+});
+
+// PUT /recipes/:recipeId
+router.put('/:recipeId', async (req, res) => {
+  try {
+    const currentRecipe = await Recipe.findById(req.params.recipeId);
+
+    if (!currentRecipe) {
+      return res.status(404).send('Recipe not found');
+    }
+
+    if (currentRecipe.owner.equals(req.session.user._id)) {
+      await currentRecipe.updateOne(req.body);
+      res.redirect('/recipes');
+    } else {
+      res.status(403).send("You don't have permission to do that.");
+    }
+  } catch (error) {
+    console.error('Error updating recipe:', error);
+    res.redirect('/');
+  }
+});
+// controllers/recipes.js
+
+router.get('/:recipeId/edit', async (req, res) => {
+    try {
+      const currentRecipe = await Recipe.findById(req.params.recipeId);
+      res.render('recipes/edit.ejs', {
+        recipe: currentRecipe,
+      });
+    } catch (error) {
+      console.log(error);
+      res.redirect('/');
+    }
+  });
+  
+
+// POST /recipes/:recipesId/favorited-by/:userId
+router.post('/:recipeId/favorited-by/:userId', async (req, res) => {
+  try {
+    await Recipe.findByIdAndUpdate(req.params.recipeId, {
+      $push: { favoritedByUsers: req.params.userId }
+    });
+    res.redirect(`/recipes/${req.params.recipeId}`);
+  } catch (error) {
+    console.error('Error favoriting recipe:', error);
+    res.redirect('/');
+  }
+});
+
+// DELETE /recipes/:recipeId/favorited-by/:userId
+router.delete('/:recipeId/favorited-by/:userId', async (req, res) => {
+  try {
+    await Recipe.findByIdAndUpdate(req.params.recipeId, {
+      $pull: { favoritedByUsers: req.params.userId }
+    });
+    res.redirect(`/recipes/${req.params.recipeId}`);
+  } catch (error) {
+    console.error('Error removing favorite:', error);
+    res.redirect('/');
+  }
+});
+
+module.exports = router;
+
